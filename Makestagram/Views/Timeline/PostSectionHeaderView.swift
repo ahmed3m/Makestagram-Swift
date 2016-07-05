cca75152780df944b46e0001435d3b3b01d439c1
ahmed3m <ahmed_3m2010@hotmail.com>
2016-07-05 10:18:14 -0400

    Add a header cell to the posts

---
+++ Makestagram/Views/Timeline/PostSectionHeaderView.swift
@@ -0,0 +1,35 @@
+//
+//  PostSectionHeaderView.swift
+//  Makestagram
+//
+//  Created by Ahmed Abdelrahman on 7/5/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PostSectionHeaderView: UITableViewCell {
+
+    @IBOutlet weak var postTimeLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    var post: Post? {
+        didSet {
+            if let post = post {
+                usernameLabel.text = post.user?.username
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
